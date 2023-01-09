f9d9573b46739284053d87291800a413292227b3
nomenklatur <dimasekaputra2000@gmail.com>
2023-01-09 13:19:17 +0700

    server updated

--- src/server.js
+++ src/server.js
@@ -1,6 +1,43 @@
-// todo buat server openmusic
-// todo buat service album
-// todo buat service songs
-// todo buat data validation
-// todo buat error handling
-// todo buat pake database
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+const songs = require('./api/songs');
+const albums = require('./api/albums');
+
+const SongsService = require('./services/SongsService');
+const AlbumsService = require('./services/AlbumsService');
+
+const init = async () => {
+  const songsService = new SongsService();
+  const albumsService = new AlbumsService();
+
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register([
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+      },
+    },
+    {
+      plugin: albums,
+      options: {
+        service: albumsService,
+      },
+    },
+  ]);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
